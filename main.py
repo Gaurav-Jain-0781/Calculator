e47860c0d4d0c1df12359b8f53e96056a81bd7ae
Gaurav-Jain-0781 <gauravjain0781@gmail.com>
2021-12-25 22:27:04 +0530

    added font properties and corrected negative operator bug

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import tkinter.font as font
 
 def operate():
     data = entry.get()
-    pattern = re.search("([0-9,.]*)([\W])([0-9]*)([\W]?)", data)
+    pattern = re.search("([\W]?[0-9,.]*)([\W])([0-9,.]*)([\W]?)", data)
     first_value = pattern[1]
     operator = pattern[2]
     second_value = pattern[3]
@@ -57,7 +57,9 @@ def display_entry(value):
 
 root = tk.Tk()
 root.title('Calculator')
-root.geometry('450x500')
+root.geometry('450x600')
+
+universal_font = font.Font(root, family="helvetica", size=15, weight="bold")
 
 entry = tk.Entry(root, justify="right")
 entry.pack(side="top", padx=4, pady=2, fill="both", ipady=35)
@@ -77,65 +79,69 @@ frame3.pack(side="bottom", fill="both", expand=True)
 frame5 = tk.Frame(root, height=50, width=50)
 frame5.pack(side="bottom", fill="both", expand=True)
 
-button_add = tk.Button(frame1, text="+", height=3, width=14, command=lambda: display_entry("+"))
+button_add = tk.Button(frame1, text="+", height=3, width=8, font=universal_font, command=lambda: display_entry("+"))
 button_add.pack(side="right", fill="both", padx=2, pady=2, expand=True)
 
-button_subtract = tk.Button(frame2, text="-", height=3, width=14, command=lambda: display_entry("-"))
+button_subtract = tk.Button(
+    frame2, text="-", height=3, width=8, font=universal_font, command=lambda: display_entry("-")
+    )
 button_subtract.pack(side="right", fill="both", padx=2, pady=2, expand=True)
 
-button_multiply = tk.Button(frame3, text="*", height=3, width=14, command=lambda: display_entry("*"))
+button_multiply = tk.Button(
+    frame3, text="*", height=3, width=8, font=universal_font, command=lambda: display_entry("*")
+    )
 button_multiply.pack(side="right", fill="both", padx=2, pady=2, expand=True)
 
-percentage = tk.Button(frame5, text="%", height="3", width="14", command=lambda: display_entry("%"))
+percentage = tk.Button(frame5, text="%", height=3, width=8, font=universal_font, command=lambda: display_entry("%"))
 percentage.pack(side="right", padx=2, pady=2, fill="both", expand=True)
 
-divide = tk.Button(frame5, text="/", height=3, width=14, command=lambda: display_entry("/"))
+divide = tk.Button(frame5, text="/", height=3, width=8, font=universal_font, command=lambda: display_entry("/"))
 divide.pack(side="right", padx="2", pady="2", fill="both", expand=True)
 
-nine = tk.Button(frame3, text="9", height=3, width=14, command=lambda: display_entry(9))
+nine = tk.Button(frame3, text="9", height=3, width=8, font=universal_font, command=lambda: display_entry(9))
 nine.pack(side="right", fill="both", padx=2, pady=2, expand=True)
 
-eight = tk.Button(frame3, text="8", height=3, width=14, command=lambda: display_entry(8))
+eight = tk.Button(frame3, text="8", height=3, width=8, font=universal_font, command=lambda: display_entry(8))
 eight.pack(side="right", fill="both", padx=2, pady=2, expand=True)
 
-seven = tk.Button(frame3, text="7", height=3, width=14, command=lambda: display_entry(7))
+seven = tk.Button(frame3, text="7", height=3, width=8, font=universal_font, command=lambda: display_entry(7))
 seven.pack(side="right", fill="both", padx=2, pady=2, expand=True)
 
-six = tk.Button(frame2, text="6", height=3, width=14, command=lambda: display_entry(6))
+six = tk.Button(frame2, text="6", height=3, width=8, font=universal_font, command=lambda: display_entry(6))
 six.pack(side="right", fill="both", padx=2, pady=2, expand=True)
 
-five = tk.Button(frame2, text="5", height=3, width=14, command=lambda: display_entry(5))
+five = tk.Button(frame2, text="5", height=3, width=8, font=universal_font, command=lambda: display_entry(5))
 five.pack(side="right", fill="both", padx=2, pady=2, expand=True)
 
-four = tk.Button(frame2, text="4", height=3, width=14, command=lambda: display_entry(4))
+four = tk.Button(frame2, text="4", height=3, width=8, font=universal_font, command=lambda: display_entry(4))
 four.pack(side="right", fill="both", padx=2, pady=2, expand=True)
 
-three = tk.Button(frame1, text="3", height=3, width=14, command=lambda: display_entry(3))
+three = tk.Button(frame1, text="3", height=3, width=8, font=universal_font, command=lambda: display_entry(3))
 three.pack(side="right", fill="both", padx=2, pady=2, expand=True)
 
-two = tk.Button(frame1, text="2", height=3, width=14, command=lambda: display_entry(2))
+two = tk.Button(frame1, text="2", height=3, width=8, font=universal_font, command=lambda: display_entry(2))
 two.pack(side="right", fill="both", padx=2, pady=2, expand=True)
 
-one = tk.Button(frame1, text="1", height=3, width=14, command=lambda: display_entry(1))
+one = tk.Button(frame1, text="1", height=3, width=8, font=universal_font, command=lambda: display_entry(1))
 one.pack(side="right", fill="both", padx=2, pady=2, expand=True)
 
-equals_to = tk.Button(frame4, text="=", height=3, width=14, command=lambda: operate())
+equals_to = tk.Button(frame4, text="=", height=3, width=8, font=universal_font, command=lambda: operate())
 equals_to.pack(side="right", fill="both", padx=2, pady=2, expand=True)
 
-point = tk.Button(frame4, text=".", height=3, width=14, command=lambda: display_entry("."))
+point = tk.Button(frame4, text=".", height=3, width=8, font=universal_font, command=lambda: display_entry("."))
 point.pack(side="right", fill="both", padx=2, pady=2, expand=True)
 
-zero = tk.Button(frame4, text="0", height=3, width=14, command=lambda: display_entry(0))
+zero = tk.Button(frame4, text="0", height=3, width=8, font=universal_font, command=lambda: display_entry(0))
 zero.pack(side="right", fill="both", padx=2, pady=2, expand=True)
 
-negative = tk.Button(frame4, text="+/-", height=3, width=14, command=lambda: display_entry("minus"))
+negative = tk.Button(frame4, text="+/-", height=3, width=8, font=universal_font, command=lambda: display_entry("minus"))
 negative.pack(side="right", fill="both", padx=2, pady=2, expand=True)
 
-clear_entry = tk.Button(frame5, text="CE", height=3, width=14, command=lambda: delete_last_entry())
+clear_entry = tk.Button(frame5, text="CE", height=3, width=8, font=universal_font, command=lambda: delete_last_entry())
 clear_entry.pack(side="right", padx=2, pady=2, fill="both", expand=True)
 
-clear_all = tk.Button(frame5, text="C", height=3, width=14, command=lambda: delete_all())
+clear_all = tk.Button(frame5, text="C", height=3, width=8, font=universal_font, command=lambda: delete_all())
 clear_all.pack(side="right", padx=2, pady=2, fill="both", expand=True)
 
-
+entry.configure(font=universal_font)
 root.mainloop()
